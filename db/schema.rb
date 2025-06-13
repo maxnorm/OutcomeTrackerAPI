99dea308d352ada8f460739545a4e8edab050a32
verrixkio <verrixkio@gmail.com>
2025-06-13 11:05:38 -0600

    test failing, end statement removed from merge conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_13_001948) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_13_154602) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -50,6 +50,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_13_001948) do
     t.jsonb "llm_analysis_raw", default: {}
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
   create_table "good_job_batches", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
