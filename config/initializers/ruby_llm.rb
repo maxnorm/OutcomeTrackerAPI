ed7475e433a3ed2597c7303cc3131b78e4ad4813
xrendan <brendan@brendansamek.com>
2025-06-14 16:52:23 -0600

    Don't require gemini api key to run

--- config/initializers/ruby_llm.rb
+++ config/initializers/ruby_llm.rb
@@ -2,7 +2,7 @@ require "ruby_llm"
 require "env_or_credentials"
 
 RubyLLM.configure do |config|
-    config.gemini_api_key = EnvOrCredentials.fetch!("GEMINI_API_KEY", :gemini_api_key)
+    config.gemini_api_key = EnvOrCredentials.fetch("GEMINI_API_KEY", :gemini_api_key)
 
     config.default_model = "gemini-2.0-flash-lite"               # Default: 'gpt-4.1-nano'
     config.default_embedding_model = "text-embedding-004"  # Default: 'text-embedding-3-small'
