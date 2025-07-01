4d7cab4a2e4dff5a509c38abfa677eb08ddd2ae8
Nick Warwick <nicholaswarwick@icloud.com>
2025-07-01 10:08:56 -0400

    Add search support for Ministers

--- app/models/minister.rb
+++ app/models/minister.rb
@@ -2,6 +2,10 @@ class Minister < ApplicationRecord
   belongs_to :government
   belongs_to :department
 
+  def self.ransackable_attributes(auth_object = nil)
+    [ "first_name", "last_name", "title" ]
+  end
+
   def compound_name
     "#{title} (#{full_name})"
   end
