8989c87ab3a05df668d979eacc6945fa7c06dabf
Nick Warwick <nicholaswarwick@icloud.com>
2025-07-02 17:26:56 -0400

    Add test for compound_name method

--- test/models/minister_test.rb
+++ test/models/minister_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
 class MinisterTest < ActiveSupport::TestCase
-  # test "that each department has a minister" do
-  #   # Ensure that each department has
-  #   assert_equal 0, Department.all.select { |d| d.minister.nil? }.count
-  # end
+  test "that compound_name is correct" do
+    minister = ministers(:finance_minister)
+    assert_equal "Deputy Prime Minister and Minister of Finance (Chrystia Freeland)", minister.compound_name
+  end
 end
